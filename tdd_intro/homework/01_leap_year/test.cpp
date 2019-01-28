9a0c6493dfe131d2af7ddb97101706cf6f3bbc0c
Dmitry Smetana <smetana@apriorit.com>
2019-01-29 01:09:59 +0200

    LeapYear: [Green] Leap year if divisible by 400. Or divisible by 4 but not 100.

--- tdd_intro/homework/01_leap_year/test.cpp
+++ tdd_intro/homework/01_leap_year/test.cpp
@@ -31,7 +31,8 @@ bool IsLeapYear(unsigned int year)
         return false;
     }
 
-    if(year % 4 == 0)
+    if((year % 400 == 0) ||
+            ((year % 4 == 0) && (year % 100 != 0)))
     {
         return true;
     }
