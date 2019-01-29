9e10ba34714ca7a5273736dd595ac62e89a37e4f
Alexander Sirenko <sirenko@dev.local>
2019-01-29 20:09:14 +0200

    Red test: Check_IsLeapYear_2000_Return_true

--- tdd_intro/homework/01_leap_year/test.cpp
+++ tdd_intro/homework/01_leap_year/test.cpp
@@ -13,3 +13,8 @@ If your language provides a method in the standard library that does this look-u
 */
 
 #include <gtest/gtest.h>
+
+TEST(LeapYearTest, Check_IsLeapYear_2000_Return_true)
+{
+    EXPECT_TRUE(IsLeapYear(2000));
+}
