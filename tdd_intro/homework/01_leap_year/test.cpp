0f88149954a07c38a173c47d85462fe783903942
Savin Dmitriy <savin.dmitriy@apriorit.com>
2019-01-30 10:51:06 +0200

    Refactoring

--- tdd_intro/homework/01_leap_year/test.cpp
+++ tdd_intro/homework/01_leap_year/test.cpp
@@ -16,7 +16,7 @@ If your language provides a method in the standard library that does this look-u
 
 bool is_leap_year(int year)
 {
-    if(year == 5 || year == 6)
+    if(year % 4 != 0)
         return false;
     return true;
 }
