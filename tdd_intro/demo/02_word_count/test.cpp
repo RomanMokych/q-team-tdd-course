0072cce16aa9ba33b6b5e7cd420e4a35fd89c724
Alexander Sirenko <sirenko@dev.local>
2019-02-12 21:41:38 +0200

    Green test

--- tdd_intro/demo/02_word_count/test.cpp
+++ tdd_intro/demo/02_word_count/test.cpp
@@ -23,26 +23,36 @@ class PhraseOccurrences
     friend bool operator==(const PhraseOccurrences& lhs, const PhraseOccurrences& rhs);
 
 public:
-    PhraseOccurrences(const std::string& word, int cout)
+    explicit PhraseOccurrences(std::map<std::string, int> phrase )
     {
-        m_occurrences[word] = cout;
+        m_occurrences.insert(phrase.begin(), phrase.end());
     }
 
-    PhraseOccurrences(const std::string& str)
+    explicit PhraseOccurrences(const std::string& str)
         : m_Phrase(str)
     {
         Process();
     }
 
 
+public:
+    void toString()
+    {
+        std::map<std::string, int>::iterator m;
+        for (m = m_occurrences.begin(); m != m_occurrences.end(); m++)
+            std::cout << m->first << " -> "
+                      << m->second << "\n";
+    }
+
 private:
     void Process()
     {
-        std::istringstream stringStream(m_Phrase);
-        std::string str;
-        while(stringStream>>str)
+
+        std::stringstream ss(m_Phrase);
+        std::string word;
+        while (ss >> word)
         {
-            m_occurrences[str]++;
+            m_occurrences[word]++;
         }
     }
 
@@ -54,7 +64,9 @@ private:
 
 bool operator==(const PhraseOccurrences& lhs, const PhraseOccurrences& rhs)
 {
-    return (lhs.m_occurrences == rhs.m_occurrences);
+    return  lhs.m_occurrences.size() == rhs.m_occurrences.size()
+            && std::equal(lhs.m_occurrences.begin(), lhs.m_occurrences.end(),
+                          rhs.m_occurrences.begin());
 }
 
 PhraseOccurrences GetPhraseOccurrences(const std::string& phrase)
@@ -64,18 +76,36 @@ PhraseOccurrences GetPhraseOccurrences(const std::string& phrase)
 
 TEST(PhraseOccurrencesTest, Test_1_word)
 {
-    PhraseOccurrences result("word", 1);
+    PhraseOccurrences result(
+                {{"word", 1}}
+                );
+
+    result.toString();
+    GetPhraseOccurrences("word").toString();
+
     EXPECT_EQ(result, GetPhraseOccurrences("word"));
 }
 
 TEST(PhraseOccurrencesTest, Test_2_word2)
 {
-    PhraseOccurrences result("word", 2);
+    PhraseOccurrences result({{"word", 2}});
     EXPECT_EQ(result, GetPhraseOccurrences("word word"));
 }
 
 TEST(PhraseOccurrencesTest, Test_3_word_occurrences1)
 {
-    PhraseOccurrences result({"word1", 1}, {"word2", 1});
+    PhraseOccurrences result({
+                                 {"word1", 1},
+                                 {"word2", 1}
+                             });
     EXPECT_EQ(result, GetPhraseOccurrences("word1 word2"));
+}
+
+TEST(PhraseOccurrencesTest, Test_3_word_occurrences2)
+{
+    PhraseOccurrences result({
+                                 {"word1", 1},
+                                 {"word2", 2}
+                             });
+    EXPECT_EQ(result, GetPhraseOccurrences("word1 word2 word2"));
 }
