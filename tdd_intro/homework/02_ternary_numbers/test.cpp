648e387c6b90c62c1b1df9b83cc713de50e09cc9
Savin Dmitriy <savin.dmitriy@apriorit.com>
2019-02-12 23:08:10 +0200

    Green test

--- tdd_intro/homework/02_ternary_numbers/test.cpp
+++ tdd_intro/homework/02_ternary_numbers/test.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-
+#include <math.h>
 /*
 Convert a ternary number, represented as a string (e.g. '102012'), to its decimal equivalent using first principles.
 
@@ -21,7 +21,13 @@ using namespace std;
 
 int ter_to_dec(const string &ternary_number)
 {
-    return stoi(ternary_number);
+    int result = 0;
+    for(int i = 0; i < ternary_number.size(); i++)
+    {
+        char val = ternary_number.at(i);
+        result += atoi(&val) * pow(3, ternary_number.size() - i - 1);
+    }
+    return result;
 }
 
 TEST(ternary_converter, 1_is_1)
