508c361eb70824febc40fb2a2423fe8ce67905da
Savin Dmitriy <savin.dmitriy@apriorit.com>
2019-03-01 09:53:15 +0200

    Red test

--- tdd_intro/homework/03_bank_ocr/test.cpp
+++ tdd_intro/homework/03_bank_ocr/test.cpp
@@ -1,6 +1,7 @@
 /*### Bank OCR
 
-Your manager has recently purchased a machine that assists in reading letters and faxes sent in by branch offices. The machine scans the paper documents, and produces a file with a number of entries. You will write a program to parse this file.
+Your manager has recently purchased a machine that assists in reading letters and faxes sent in by branch offices.
+The machine scans the paper documents, and produces a file with a number of entries. You will write a program to parse this file.
 
 #### Specification
 #### User Story 1
@@ -11,9 +12,11 @@ The following format is created by the machine:
   | _| _||_||_ |_   ||_||_|
   ||_  _|  | _||_|  ||_| _|
 ```
-Each entry is 3 lines long, and each line has 27 characters. 3 lines of each entry contain an account number written using pipes and underscores.
+Each entry is 3 lines long, and each line has 27 characters.
+3 lines of each entry contain an account number written using pipes and underscores.
 
-Each account number should have 9 digits, all of which should be in the range 0-9. A normal file contains around 500 entries.
+Each account number should have 9 digits, all of which should be in the range 0-9.
+A normal file contains around 500 entries.
 
 Write a program that can take this file and parse it into actual account numbers.
 
@@ -195,3 +198,14 @@ const Display s_display123456789 = { "    _  _     _  _  _  _  _ ",
                                      "  | _| _||_||_ |_   ||_||_|",
                                      "  ||_  _|  | _||_|  ||_| _|"
 };
+
+std::vector<std::string> split_to_3_strings(std::string str)
+{
+    return {};
+}
+
+TEST(Split, abc_splited_to_a_b_c)
+{
+    std::vector<std::string> result = {"a", "b", "c"};
+    EXPECT_EQ(result, split_to_3_strings("abc"));
+}
